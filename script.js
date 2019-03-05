b10bc0197475c7daa04e03bf38ee9fe699ffa385
EllaUsik <usik.ella@gmail.com>
2019-03-05 17:53:11 +0300

    add js exercises

--- script.js
+++ script.js
@@ -89,3 +89,37 @@ function valueChanging ( prevInputValue, newInputValue) {
 
 valueChanging (firstInputValue, secondInputValue);
 valueChanging (secondInputValue, firstInputValue);*/
+
+/*for (var i = 0; i < 3; i++) {
+  alert( "номер " + i + "!" );
+}
+
+var i = 0;
+
+while (i < 3) {
+  alert( "номер " + i + "!" );
+  i++;
+}*/
+
+
+/*var enteringNumber = prompt('enter the number more than 100', ' ');
+
+while (enteringNumber < 100 && enteringNumber != null) {
+  var newEnteringNumber = prompt('once again enter the number more than 100', '101');
+  if (newEnteringNumber > 100 || enteringNumber === null) break;
+}*/
+
+/*Натуральное число, большее 1, называется простым, если оно ни на что не делится, кроме себя и 1.
+
+Другими словами, n>1 – простое, если при делении на любое число от 2 до n-1 есть остаток.
+
+Создайте код, который выводит все простые числа из интервала от 2 до 10. Результат должен быть: 2,3,5,7.
+
+*/
+nextNumber:
+for (var n = 2; n <= 10; n++) {
+  for (var i = 2; i <= n-1; i++) {
+    if (n%i == 0) continue nextNumber;
+  }
+  alert(n);
+}
