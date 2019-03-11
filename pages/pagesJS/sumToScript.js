3bc199c31ad8454c1bc15b4ded1cbee4b26854b7
EllaUsik <usik.ella@gmail.com>
2019-03-11 17:24:27 +0300

    add methods of count

--- pages/pagesJS/sumToScript.js
+++ pages/pagesJS/sumToScript.js
@@ -6,26 +6,31 @@ var sumButtonByFormula = document.getElementById("button3");
 console.log(sumButtonByFunction);
 
 var sum, n;
-sumButtonByFunction.addEventListener('click', sumToByFunction);
-sumButtonByRecursion.addEventListener('click', sumToByRecursion);
-sumButtonByFormula.addEventListener('click', sumToByFormula);
 
-function sumToByFunction(n) {
+sumButtonByFunction.addEventListener('click', countSumToByFunction);
+sumButtonByRecursion.addEventListener('click', countSumToByRecursion);
+sumButtonByFormula.addEventListener('click', countSumToByFormula);
+
+function countSumToByFunction(n) {
   n = +prompt('enter N:', '');
   sum = 0;
   for (var i = 1; i <= n ; i++) {
     sum += i;
   }
-  return sum;
-}
-console.log(sum);
-
-function sumToByRecursion(n) {
-
+  alert ('sum from 1 to N: ' + sum);
 }
 
-function sumToByFormula(n) {
-
+function countSumToByRecursion(n) {
+  n = +prompt('enter N:', '');
+   if (n > 0) {
+     return n + countSumToByRecursion(n - 1);
+   }
+   var sumByRecursion = countSumToByRecursion();
+   alert(sumByRecursion);
 }
 
-alert ('sum from 1 to N: ' + sum);
+function countSumToByFormula(n) {
+  n = +prompt('enter N:', '');
+  sum = n * (n + 1) / 2;
+  alert ('sum from 1 to N: ' + sum);
+}
